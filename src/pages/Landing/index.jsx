726be64b3071ab79f696248215ea113367d8b589
Olmeiro Orozco <33037061+olmeiro@users.noreply.github.com>
2023-06-02 00:10:00 -0500

    Feature/signin design (#7)
    * feat: routing app
    
    Routing app with react-router-dom v6
    
    * first sketch footer
    
    * feat: footer design
    
    First approach to design footer component
    
    * feat: Landing design
    
    first approach landing design component
    
    * refactor: change paragrahp to Reading Wishlist
    
    * feat: optional menu desktop button
    
    * feat: optional navbar menu
    
    * feat: design login page
    
    * feat: login design
    
    First approach login page, in fact could be great to define Navbar to this component. I'm stucked trying to put an image inside

--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
-import Footer from '../../components/Footer';
 
+import Footer from '../../components/Footer';
 import Navbar from '../../components/Navbar';
 
 const Landing = () => {
--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,18 +1,73 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import Navbar from '../../components/Navbar';
+import Footer from '../../components/Footer';
 
 const SignIn = () => {
   return (
-    <nav className="w-full">
-      <h2 className="flex justify-center">Sign In page</h2>
-      <ul className="flex flex-row justify-end">
-        <li className="m-4">
-          <Link to="/">Go Home after login</Link>
-        </li>
-        <li className="m-4">
-          <Link to="sign-up">Sign Up</Link>
-        </li>
-      </ul>
-    </nav>
+    <div className="flex flex-col">
+      <Navbar />
+      <div className="flex flex-col sm:m-16 md:m-0">
+        <div className="w-full max-w-md mx-auto md:mt-28">
+          <div className="flex flex-col">
+            <div>
+              <h2 className="text-4xl text-black text-center">
+                Let's get started!
+              </h2>
+            </div>
+          </div>
+          <form>
+            <div className="mt-4 space-y-6">
+              <div className="col-span-full">
+                <label
+                  className="block mb-3 text-sm font-medium text-gray-600"
+                  name="email"
+                >
+                  Email
+                </label>
+                <input
+                  className="block w-full px-6 py-3 text-black bg-white border border-gray-200 appearance-none rounded-xl placeholder:text-gray-400 focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
+                  placeholder="email@example.com"
+                  autocomplete="off"
+                  type="email"
+                />
+              </div>
+              <div className="col-span-full">
+                <label
+                  className="block mb-3 text-sm font-medium text-gray-600"
+                  name="company"
+                >
+                  Password
+                </label>
+                <input
+                  className="block w-full px-6 py-3 text-black bg-white border border-gray-200 appearance-none rounded-xl placeholder:text-gray-400 focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
+                  placeholder="Password"
+                  type="password"
+                />
+              </div>
+              <div className="col-span-full">
+                <button
+                  className="items-center justify-center w-full px-6 py-2.5 text-center text-white duration-200 bg-black border-2 border-black rounded-full nline-flex hover:bg-transparent hover:border-black hover:text-black focus:outline-none focus-visible:outline-black text-sm focus-visible:ring-black"
+                  type="submit"
+                >
+                  Login
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+        <div className="col-span-full">
+          <NavLink to="/sign-up">
+            <label
+              className="block mt-8 text-center text-sm font-medium text-blue-500"
+              name="email"
+            >
+              Don't have account yet?
+            </label>
+          </NavLink>
+        </div>
+      </div>
+      <Footer />
+    </div>
   );
 };
 export default SignIn;
